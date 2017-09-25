c7ed6b6fc8cd8a94608d4ae0880d67fc66f17ed4
Roar Fredriksen <roarfred@gmail.com>
2017-09-25 22:58:45 +0200

    Adjusted project to prepare for library

--- Code/ESPDebugger/ESPDebugger.ino
+++ Code/ESPDebugger/ESPDebugger.ino
@@ -11,9 +11,14 @@
  * 
  * Created 14. september 2017 by Roar Fredriksen
  */
+
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
+#include "HanReader.h"
+
+// The HAN Port reader
+HanReader hanReader;
 
 // WiFi and MQTT endpoints
 const char* ssid     = "Roar_Etne";
@@ -23,151 +28,164 @@ const char* mqtt_server = "192.168.10.203";
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-#include "DlmsReader.h"
-#include "KaifaHan.h"
-
-DlmsReader reader;
-byte buffer[512];
-int bytesRead;
-KaifaHan kaifa;
-
 void setup() {
-  // Initialize the Serial1 port for debugging
-  // (This port is fixed to Pin2 of the ESP8266)
-  Serial1.begin(115200);
-  while (!Serial1) {}
-  Serial1.setDebugOutput(true);
-  Serial1.println("Serial1");
-  Serial1.println("Serial debugging port initialized");
-
-  // Initialize wifi
-  Serial1.print("Connecting to ");
-  Serial1.println(ssid);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
+	setupDebugPort();
+	setupWiFi();
+	setupMqtt();
   
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial1.print(".");
-  }
-  
-  Serial1.println("");
-  Serial1.println("WiFi connected");  
-  Serial1.println("IP address: ");
-  Serial1.println(WiFi.localIP());
+	// initialize the HanReader
+	// (passing Serial as the HAN port and Serial1 for debugging)
+	hanReader.setup(&Serial, &Serial1);
+}
 
-  client.setServer(mqtt_server, 1883);
-  
-  // Initialize H/W serial port for MBus communication
-  Serial.begin(2400, SERIAL_8E1);
-  while (!Serial) {}
-  Serial1.println("MBUS serial setup complete");
+void setupMqtt()
+{
+	client.setServer(mqtt_server, 1883);
+}
 
-  bytesRead = 0;
+void setupDebugPort()
+{
+	// Initialize the Serial1 port for debugging
+	// (This port is fixed to Pin2 of the ESP8266)
+	Serial1.begin(115200);
+	while (!Serial1) {}
+	Serial1.setDebugOutput(true);
+	Serial1.println("Serial1");
+	Serial1.println("Serial debugging port initialized");
+}
+
+void setupWiFi()
+{
+	// Initialize wifi
+	Serial1.print("Connecting to ");
+	Serial1.println(ssid);
+	WiFi.mode(WIFI_STA);
+	WiFi.begin(ssid, password);
+
+	while (WiFi.status() != WL_CONNECTED) {
+		delay(500);
+		Serial1.print(".");
+	}
+
+	Serial1.println("");
+	Serial1.println("WiFi connected");
+	Serial1.println("IP address: ");
+	Serial1.println(WiFi.localIP());
 }
 
 void loop() {
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();  
-  
-  if (Serial.available())
-  {
-    byte newByte = Serial.read();
-    //if (newByte < 0x10) Serial1.print("0");
-    //Serial1.print(newByte, HEX);
+	loopMqtt();
 
-    if (reader.Read(newByte))
-    {
-      bytesRead = reader.GetRawData(buffer, 0, 512);
+	// Read one byt from the port, and see if we got a full package
+	if (hanReader.read())
+	{
+		// Get the list identifier
+		List list = hanReader.getList();
+		Serial1.println("");
+		Serial1.print("List #");
+		Serial1.print((byte)list, HEX);
+		Serial1.print(": ");
 
-      byte list = kaifa.GetListID(buffer, 0, bytesRead);
-      Serial1.println("");
-      Serial1.print("List #");
-      Serial1.print(list, HEX);
-      Serial1.print(": ");
-      
-      time_t time = kaifa.GetPackageTime(buffer, 0, bytesRead);
+		// Make sure we got a valid list
+		if (list == List::ListUnknown)
+		{
+			Serial1.println("Invalid list");
+			return;
+		}
 
-      StaticJsonBuffer<500> jsonBuffer;
-      JsonObject& root = jsonBuffer.createObject();
-      root["id"] = "espdebugger";
-      root["up"] = millis();
-      root["t"] = time;
-      
-      JsonObject& data = root.createNestedObject("data");
-      
-      if (list == (byte)List::List1)
-      {
-        data["P"] = kaifa.GetInt((int)List1_ObisObjects::ActivePowerImported, buffer, 0, bytesRead);
-      }
-      else if (list == (byte)List::List2)
-      {
-        data["lv"] = kaifa.GetString((int)List2_ObisObjects::ObisListVersionIdentifier, buffer, 0, bytesRead);
-        data["id"] = kaifa.GetString((int)List2_ObisObjects::MeterID, buffer, 0, bytesRead);
-        data["type"] = kaifa.GetString((int)List2_ObisObjects::MeterType, buffer, 0, bytesRead);
-        data["P"] = kaifa.GetInt((int)List2_ObisObjects::ActivePowerImported, buffer, 0, bytesRead);
-        data["Q"] = kaifa.GetInt((int)List2_ObisObjects::ReactivePowerImported, buffer, 0, bytesRead);
-        data["I1"] = kaifa.GetInt((int)List2_ObisObjects::CurrentPhaseL1, buffer, 0, bytesRead);
-        data["I2"] = kaifa.GetInt((int)List2_ObisObjects::CurrentPhaseL2, buffer, 0, bytesRead);
-        data["I3"] = kaifa.GetInt((int)List2_ObisObjects::CurrentPhaseL3, buffer, 0, bytesRead);
-        data["U1"] = kaifa.GetInt((int)List2_ObisObjects::VoltagePhaseL1, buffer, 0, bytesRead);
-        data["U2"] = kaifa.GetInt((int)List2_ObisObjects::VoltagePhaseL1, buffer, 0, bytesRead);
-        data["U3"] = kaifa.GetInt((int)List2_ObisObjects::VoltagePhaseL1, buffer, 0, bytesRead);
-      }
-      else if (list == (byte)List::List3)
-      {
-        data["lv"] = kaifa.GetString((int)List3_ObisObjects::ObisListVersionIdentifier, buffer, 0, bytesRead);
-        data["id"] = kaifa.GetString((int)List3_ObisObjects::MeterID, buffer, 0, bytesRead);
-        data["type"] = kaifa.GetString((int)List3_ObisObjects::MeterType, buffer, 0, bytesRead);
-        data["P"] = kaifa.GetInt((int)List3_ObisObjects::ActivePowerImported, buffer, 0, bytesRead);
-        data["Q"] = kaifa.GetInt((int)List3_ObisObjects::ReactivePowerImported, buffer, 0, bytesRead);
-        data["I1"] = kaifa.GetInt((int)List3_ObisObjects::CurrentPhaseL1, buffer, 0, bytesRead);
-        data["I2"] = kaifa.GetInt((int)List3_ObisObjects::CurrentPhaseL2, buffer, 0, bytesRead);
-        data["I3"] = kaifa.GetInt((int)List3_ObisObjects::CurrentPhaseL3, buffer, 0, bytesRead);
-        data["U1"] = kaifa.GetInt((int)List3_ObisObjects::VoltagePhaseL1, buffer, 0, bytesRead);
-        data["U2"] = kaifa.GetInt((int)List3_ObisObjects::VoltagePhaseL1, buffer, 0, bytesRead);
-        data["U3"] = kaifa.GetInt((int)List3_ObisObjects::VoltagePhaseL1, buffer, 0, bytesRead);
-        data["tPI"] = kaifa.GetInt((int)List3_ObisObjects::TotalActiveEnergyImported, buffer, 0, bytesRead);
-        data["tPO"] = kaifa.GetInt((int)List3_ObisObjects::TotalActiveEnergyExported, buffer, 0, bytesRead);
-        data["tQI"] = kaifa.GetInt((int)List3_ObisObjects::TotalReactiveEnergyImported, buffer, 0, bytesRead);
-        data["tQO"] = kaifa.GetInt((int)List3_ObisObjects::TotalReactiveEnergyExported, buffer, 0, bytesRead);
-      }
-      else
-      {
-        Serial1.println("Invalid list");
-        return;
-      }
+		// Get the timestamp (as unix time) from the package
+		time_t time = hanReader.getPackageTime();
 
-      root.printTo(Serial1);
-      Serial1.println();
+		// Define a json object to keep the data
+		StaticJsonBuffer<500> jsonBuffer;
+		JsonObject& root = jsonBuffer.createObject();
+		
+		// Any generic useful info here
+		root["id"] = "espdebugger";
+		root["up"] = millis();
+		root["t"] = time;
 
-      char msg[1024];
-      root.printTo(msg, 1024);
-      client.publish("sensors/out/espdebugger", msg);
-    }
-  }
+		// Add a sub-structure to the json object, 
+		// to keep the data from the meter itself
+		JsonObject& data = root.createNestedObject("data");
+		
+		// Based on the list number, get all details 
+		// according to OBIS specifications for the meter
+		if (list == List::List1)
+		{
+			data["P"] = hanReader.getInt(List1_ObisObjects::ActivePowerImported);
+		}
+		else if (list == List::List2)
+		{
+			data["lv"] = hanReader.getString(List2_ObisObjects::ObisListVersionIdentifier);
+			data["id"] = hanReader.getString(List2_ObisObjects::MeterID);
+			data["type"] = hanReader.getString(List2_ObisObjects::MeterType);
+			data["P"] = hanReader.getInt(List2_ObisObjects::ActivePowerImported);
+			data["Q"] = hanReader.getInt(List2_ObisObjects::ReactivePowerImported);
+			data["I1"] = hanReader.getInt(List2_ObisObjects::CurrentPhaseL1);
+			data["I2"] = hanReader.getInt(List2_ObisObjects::CurrentPhaseL2);
+			data["I3"] = hanReader.getInt(List2_ObisObjects::CurrentPhaseL3);
+			data["U1"] = hanReader.getInt(List2_ObisObjects::VoltagePhaseL1);
+			data["U2"] = hanReader.getInt(List2_ObisObjects::VoltagePhaseL1);
+			data["U3"] = hanReader.getInt(List2_ObisObjects::VoltagePhaseL1);
+		}
+		else if (list == List::List3)
+		{
+			data["lv"] = hanReader.getString(List3_ObisObjects::ObisListVersionIdentifier);;
+			data["id"] = hanReader.getString(List3_ObisObjects::MeterID);
+			data["type"] = hanReader.getString(List3_ObisObjects::MeterType);
+			data["P"] = hanReader.getInt(List3_ObisObjects::ActivePowerImported);
+			data["Q"] = hanReader.getInt(List3_ObisObjects::ReactivePowerImported);
+			data["I1"] = hanReader.getInt(List3_ObisObjects::CurrentPhaseL1);
+			data["I2"] = hanReader.getInt(List3_ObisObjects::CurrentPhaseL2);
+			data["I3"] = hanReader.getInt(List3_ObisObjects::CurrentPhaseL3);
+			data["U1"] = hanReader.getInt(List3_ObisObjects::VoltagePhaseL1);
+			data["U2"] = hanReader.getInt(List3_ObisObjects::VoltagePhaseL1);
+			data["U3"] = hanReader.getInt(List3_ObisObjects::VoltagePhaseL1);
+			data["tPI"] = hanReader.getInt(List3_ObisObjects::TotalActiveEnergyImported);
+			data["tPO"] = hanReader.getInt(List3_ObisObjects::TotalActiveEnergyExported);
+			data["tQI"] = hanReader.getInt(List3_ObisObjects::TotalReactiveEnergyImported);
+			data["tQO"] = hanReader.getInt(List3_ObisObjects::TotalReactiveEnergyExported);
+		}
+
+		// Write the json to the debug port
+		root.printTo(Serial1);
+		Serial1.println();
+
+		// Publish the json to the MQTT server
+		char msg[1024];
+		root.printTo(msg, 1024);
+		client.publish("sensors/out/espdebugger", msg);
+	}
 }
 
+// Ensure the MQTT lirary gets some attention too
+void loopMqtt()
+{
+	if (!client.connected()) {
+		reconnectMqtt();
+	}
+	client.loop();
+}
 
-void reconnect() {
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial1.print("Attempting MQTT connection...");
-    // Attempt to connect
-    if (client.connect("ESP8266Client")) {
-      Serial1.println("connected");
-      // Once connected, publish an announcement...
-      // client.publish("sensors", "hello world");
-      // ... and resubscribe
-      // client.subscribe("inTopic");
-    } else {
-      Serial1.print("failed, rc=");
-      Serial1.print(client.state());
-      Serial1.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
+void reconnectMqtt() {
+	// Loop until we're reconnected
+	while (!client.connected()) {
+		Serial1.print("Attempting MQTT connection...");
+		// Attempt to connect
+		if (client.connect("ESP8266Client")) {
+			Serial1.println("connected");
+			// Once connected, publish an announcement...
+			// client.publish("sensors", "hello world");
+			// ... and resubscribe
+			// client.subscribe("inTopic");
+		}
+		else {
+			Serial1.print("failed, rc=");
+			Serial1.print(client.state());
+			Serial1.println(" try again in 5 seconds");
+			// Wait 5 seconds before retrying
+			delay(5000);
+		}
+	}
 }

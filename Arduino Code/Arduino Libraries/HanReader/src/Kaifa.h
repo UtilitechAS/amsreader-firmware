bf83f018f39ae3b6d797629bb06440cbf8893f4d
Mona Lisagit config --global user.email <stedy6@hotmail.com>
2018-09-02 16:45:06 +0200

    compile on d1 mini and working on  1 fase nuri

--- Arduino Code/Arduino Libraries/HanReader/src/Kaifa.h
+++ Arduino Code/Arduino Libraries/HanReader/src/Kaifa.h
@@ -2,15 +2,29 @@
 #define _KAIFA_h
 
 enum class Kaifa : byte {
-	List1 = 0x01,
-	List21 = 0x09,
-	List2 = 0x0D,
-	List3 = 0x12
+	List1  = 0x01,
+	List21 = 0x09, // list 2 for 1 fase
+	List2  = 0x0D, // list 2 for 3 fase
+	List31 = 0x0E, // list 3 for 1 fase
+	List3  = 0x12  // list 3 for 3 fase
 };
 
 enum class Kaifa_List1 {
 	ListSize,
 	ActivePowerImported
+};
+
+enum class Kaifa_List21 {
+	ListSize,
+	ListVersionIdentifier,
+	MeterID,
+	MeterType,
+	ActiveImportPower,
+	ActiveExportPower,
+	ReactiveImportPower,
+	ReactiveExportPower,
+	CurrentL1,
+	VoltageL1
 };
 
 enum class Kaifa_List2 {
@@ -28,6 +42,24 @@ enum class Kaifa_List2 {
 	VoltageL1,
 	VoltageL2,
 	VoltageL3
+};
+
+enum class Kaifa_List31 {
+	ListSize,
+	ListVersionIdentifier,
+	MeterID,
+	MeterType,
+	ActiveImportPower,
+	ActiveExportPower,
+	ReactiveImportPower,
+	ReactiveExportPower,
+	CurrentL1,
+	VoltageL1,
+	MeterClock,
+	CumulativeActiveImportEnergy,
+	CumulativeActiveExportEnergy,
+	CumulativeReactiveImportEnergy,
+	CumulativeReactiveExportEnergy
 };
 
 enum class Kaifa_List3 {

2681ab0548548d47766ecbe36c59cfe806736a65
Vegar Westerlund <vegarwe@gmail.com>
2019-06-06 23:58:34 +0200

    Make com port a input argument

--- Debugging/Code/SerialSimulator/ams_serial_simulator.py
+++ Debugging/Code/SerialSimulator/ams_serial_simulator.py
@@ -4,10 +4,35 @@ import sys
 
 import sample_data
 
-with serial.Serial(port='COM15', baudrate=2400, bytesize=8, parity='E', stopbits=1) as ser:
-    for packet in sample_data.kaifa_20190508_packets:
-        print('sleeping')
-        sys.stdout.flush()
-        time.sleep(2)
-        ser.write(packet)
+serial_port = sys.argv[1]
 
+with serial.Serial(port=serial_port, baudrate=2400, bytesize=8, parity='E', stopbits=1) as ser:
+    time.sleep(1)
+    while True:
+        for packet in sample_data.kaifa_20190508_packets[:]:
+            sys.stdout.flush()
+            ser.write(packet)
+            print('sleeping')
+            time.sleep(2)
+        break
+
+
+# POST  HTTP/1.1
+# Host: kanskje.de:8181
+# User-Agent: ESP32HTTPClient
+# Connection: close
+# Accept-Encoding: identity;q=1,chunked;q=0.1,*;q=0
+# Authorization: Basic dmVnYXJ3ZTpmaXNrZW4=
+# Content-Type: application/json
+# Content-Length: 44
+# 
+# {"up":88695,"t":1557354754,"data":{"P":931}}
+
+# POST / HTTP/1.1
+# Host: kanskje.de:8181
+# User-Agent: curl/7.64.0
+# Accept: */*
+# Content-Type: application/json
+# Content-Length: 44
+# 
+# {"up":88695,"t":1557354754,"data":{"P":931}}

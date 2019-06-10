1583b543bfca352d52b6a9f7de501f358548bac7
Vegar Westerlund <vegarwe@gmail.com>
2019-06-10 16:25:29 +0200

    Minor test tool cleanup

--- Debugging/Code/SerialSimulator/ams_serial_simulator.py
+++ Debugging/Code/SerialSimulator/ams_serial_simulator.py
@@ -7,6 +7,8 @@ import sample_data
 serial_port = sys.argv[1]
 
 with serial.Serial(port=serial_port, baudrate=2400, bytesize=8, parity='E', stopbits=1) as ser:
+    print(ser)
+    print('sleeping')
     time.sleep(1)
     while True:
         for packet in sample_data.kaifa_20190508_packets[:]:
@@ -15,24 +17,3 @@ with serial.Serial(port=serial_port, baudrate=2400, bytesize=8, parity='E', stop
             print('sleeping')
             time.sleep(2)
         break
-
-
-# POST  HTTP/1.1
-# Host: kanskje.de:8181
-# User-Agent: ESP32HTTPClient
-# Connection: close
-# Accept-Encoding: identity;q=1,chunked;q=0.1,*;q=0
-# Authorization: Basic dmVnYXJ3ZTpmaXNrZW4=
-# Content-Type: application/json
-# Content-Length: 44
-# 
-# {"up":88695,"t":1557354754,"data":{"P":931}}
-
-# POST / HTTP/1.1
-# Host: kanskje.de:8181
-# User-Agent: curl/7.64.0
-# Accept: */*
-# Content-Type: application/json
-# Content-Length: 44
-# 
-# {"up":88695,"t":1557354754,"data":{"P":931}}

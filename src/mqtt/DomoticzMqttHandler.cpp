e929f87ea9539dba371d0f2fc44d5a358838994c
Gunnar Skjold <gunnar@origin.no>
2021-10-23 10:38:53 +0200

    Fixed decimals in json

--- src/mqtt/DomoticzMqttHandler.cpp
+++ src/mqtt/DomoticzMqttHandler.cpp
@@ -23,55 +23,47 @@ bool DomoticzMqttHandler::publish(AmsData* data, AmsData* previousState) {
         return ret;
 
     if (config.vl1idx > 0){				
-        if (data->getL1Voltage() > 0.1){ 
-            char val[16];
-            snprintf(val, 16, "%.2f", data->getL1Voltage());
-            char json[192];
-            snprintf_P(json, sizeof(json), DOMOTICZ_JSON,
-                config.vl1idx,
-                val
-            );
-            ret |= mqtt->publish("domoticz/in", json);
-        }
+        char val[16];
+        snprintf(val, 16, "%.2f", data->getL1Voltage());
+        char json[192];
+        snprintf_P(json, sizeof(json), DOMOTICZ_JSON,
+            config.vl1idx,
+            val
+        );
+        ret |= mqtt->publish("domoticz/in", json);
     }
 
     if (config.vl2idx > 0){				
-        if (data->getL2Voltage() > 0.1){ 
-            char val[16];
-            snprintf(val, 16, "%.2f", data->getL2Voltage());
-            char json[192];
-            snprintf_P(json, sizeof(json), DOMOTICZ_JSON,
-                config.vl2idx,
-                val
-            );
-            ret |= mqtt->publish("domoticz/in", json);
-        }
+        char val[16];
+        snprintf(val, 16, "%.2f", data->getL2Voltage());
+        char json[192];
+        snprintf_P(json, sizeof(json), DOMOTICZ_JSON,
+            config.vl2idx,
+            val
+        );
+        ret |= mqtt->publish("domoticz/in", json);
     }
 
     if (config.vl3idx > 0){				
-        if (data->getL3Voltage() > 0.1){ 
-            char val[16];
-            snprintf(val, 16, "%.2f", data->getL3Voltage());
-            char json[192];
-            snprintf_P(json, sizeof(json), DOMOTICZ_JSON,
-                config.vl3idx,
-                val
-            );
-            ret |= mqtt->publish("domoticz/in", json);
-        }
+        char val[16];
+        snprintf(val, 16, "%.2f", data->getL3Voltage());
+        char json[192];
+        snprintf_P(json, sizeof(json), DOMOTICZ_JSON,
+            config.vl3idx,
+            val
+        );
+        ret |= mqtt->publish("domoticz/in", json);
     }
 
     if (config.cl1idx > 0){				
-        if(data->getL1Current() > 0.0) {
-            char val[16];
-            snprintf(val, 16, "%.1f;%.1f;%.1f", data->getL1Current(), data->getL2Current(), data->getL3Current());
-            char json[192];
-            snprintf_P(json, sizeof(json), DOMOTICZ_JSON,
-                config.cl1idx,
-                val
-            );
-            ret |= mqtt->publish("domoticz/in", json);
-        }
+        char val[16];
+        snprintf(val, 16, "%.1f;%.1f;%.1f", data->getL1Current(), data->getL2Current(), data->getL3Current());
+        char json[192];
+        snprintf_P(json, sizeof(json), DOMOTICZ_JSON,
+            config.cl1idx,
+            val
+        );
+        ret |= mqtt->publish("domoticz/in", json);
     }			
     return ret;
 }

94a64081c5f9bfa738066300bbc0d76f4c826727
Daniel Ekman <knegge@gmail.com>
2022-01-14 00:07:46 +0100

    increase watchdog time to 60s for ota

--- src/AmsToMqttBridge.ino
+++ src/AmsToMqttBridge.ino
@@ -13,10 +13,10 @@
  */
 #if defined(ESP8266)
 ADC_MODE(ADC_VCC);
-#else if defined(ESP32)
+#elif defined(ESP32)
 #include <esp_task_wdt.h>
 #endif
-#define WDT_TIMEOUT 10
+#define WDT_TIMEOUT 60
 
 #include "AmsToMqttBridge.h"
 #include "AmsStorage.h"

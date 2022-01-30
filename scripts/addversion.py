7c1ee20e7bd862c2f5b6a99cdb05cc15b0330616
Gunnar Skjold <gunnar@origin.no>
2022-01-30 11:10:30 +0100

    Commit hash as version

--- scripts/addversion.py
+++ scripts/addversion.py
@@ -4,8 +4,14 @@ import subprocess
 FILENAME_VERSION_H = 'src/version.h'
 version = os.environ.get('GITHUB_TAG')
 if version == None:
-    result = subprocess.run(['git','rev-parse','--short','HEAD'], stdout=subprocess.PIPE)
-    version = result.stdout.decode('utf-8').strip()
+    try:
+      result = subprocess.run(['git','rev-parse','--short','HEAD'], capture_output=True, check=False)
+      if result.returncode == 0:
+        version = result.stdout.decode('utf-8').strip()
+      else:
+        version = "SNAPSHOT"
+    except:
+      version = "SNAPSHOT"
 
 import datetime
 

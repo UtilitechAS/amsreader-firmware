06ec97b42a1f31d56f562ae3f8c654422f549af6
Gunnar Skjold <gunnar@origin.no>
2022-10-06 17:20:13 +0200

    Make sure we dont use more than 5 peaks if defined higher

--- src/mqtt/HomeAssistantMqttHandler.cpp
+++ src/mqtt/HomeAssistantMqttHandler.cpp
@@ -62,7 +62,7 @@ bool HomeAssistantMqttHandler::publish(AmsData* data, AmsData* previousState, En
     if(peakCount > 5) peakCount = 5;
 	for(uint8_t i = 1; i <= peakCount; i++) {
 		if(!peaks.isEmpty()) peaks += ",";
-		peaks += String(ea->getPeak(i));
+		peaks += String(ea->getPeak(i), 2);
 	}
     snprintf_P(json, BufferSize, REALTIME_JSON,
 		ea->getMonthMax(),

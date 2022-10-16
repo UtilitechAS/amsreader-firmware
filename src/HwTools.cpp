34ebe9601ac76d32a7842ee4473ee3d962e9b61f
Gunnar Skjold <gunnar@origin.no>
2022-10-16 18:31:11 +0200

    Changed blink behaviour

--- src/HwTools.cpp
+++ src/HwTools.cpp
@@ -371,10 +371,10 @@ bool HwTools::ledOff(uint8_t color) {
 bool HwTools::ledBlink(uint8_t color, uint8_t blink) {
     for(int i = 0; i < blink; i++) {
         if(!ledOn(color)) return false;
-        delay(150);
+        delay(50);
         ledOff(color);
         if(i != blink-1)
-            delay(250);
+            delay(200);
     }
     return true;
 }

39b68aca514993cbcdd87e02d42789151a4bfe6f
Gunnar Skjold <gunnar@origin.no>
2022-10-18 18:30:26 +0200

    Removed invalid code

--- src/IEC6205675.cpp
+++ src/IEC6205675.cpp
@@ -371,13 +371,11 @@ IEC6205675::IEC6205675(const char* d, uint8_t useMeterType, MeterConfig* meterCo
                         memcpy(str, mid->oct.data, mid->oct.length);
                         str[mid->oct.length] = 0x00;
                         meterId = String(str);
-                        meterId[mid->str.length] = 0;
                         break;
                     case CosemTypeOctetString:
                         memcpy(str, mid->str.data, mid->str.length);
                         str[mid->str.length] = 0x00;
                         meterId = String(str);
-                        meterId[mid->str.length] = 0;
                         break;
                 }
             }

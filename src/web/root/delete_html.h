ba54f8934d3f6bf461410179f93a48a8ace36589
ehorvat1 <engelbert.horvat@gmx.net>
2022-11-30 21:32:03 +0100

    Add files via upload

---
+++ src/web/root/delete_html.h
@@ -0,0 +1,2 @@
+static const char DELETE_HTML[] PROGMEM = R"=="==(<form method=post > <div class="my-3 p-3 bg-white rounded shadow"> <div class="alert alert-warning">Are you sure you want to delete this file?</div> </div> <hr/> <div class="row form-group"> <div class=col-6 > <a href="javascript:history.back();" class="btn btn-outline-secondary">Back</a> </div> <div class="col-6 text-right"> <button class="btn btn-danger">Delete</button> </div> </div> </form>)=="==";
+const int DELETE_HTML_LEN PROGMEM = 396;
---
+++ src/web/root/domoticz_json.h
@@ -0,0 +1,2 @@
+static const char DOMOTICZ_JSON[] PROGMEM = R"=="==({"command":"udevice","idx":%d,"nvalue":0,"svalue":"%s"})=="==";
+const int DOMOTICZ_JSON_LEN PROGMEM = 55;

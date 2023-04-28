1e88148971eda753361f08f7ee68dee30447cf38
Gunnar Skjold <gunnar.skjold@gmail.com>
2023-04-28 18:42:49 +0200

    Fixed flash script

--- scripts/esp32/flash.sh
+++ scripts/esp32/flash.sh
@@ -29,7 +29,7 @@ fi
 
 if [ "$1" = "flash" ];then
     $esptool --chip esp32 --port $2 --baud 460800 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect \
-    0x1000 bootloader_dio_40m.bin \
+    0x1000 bootloader.bin \
     0x8000 partitions.bin \
     0xe000 boot_app0.bin \
     0x10000 firmware.bin
--- scripts/esp32s2/mkzip.sh
+++ scripts/esp32s2/mkzip.sh
@@ -8,7 +8,6 @@ if [ ! -d $build_dir ];then
     exit 1
 fi
 
-cp ~/.platformio/packages/framework-arduinoespressif32/tools/sdk/$env/bin/bootloader_qio_40m.bin $build_dir
 cp ~/.platformio/packages/framework-arduinoespressif32/tools/partitions/boot_app0.bin $build_dir
 chmod +x scripts/$env/flash.sh
 zip -j $env.zip $build_dir/*.bin scripts/$env/flash.sh

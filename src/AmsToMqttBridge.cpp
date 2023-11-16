6d4d901dba6446773d90c2ce78e4d5fbe16b9891
Gunnar Skjold <gunnar.skjold@gmail.com>
2023-11-16 17:59:33 +0100

    Migrating to new license

--- src/AmsToMqttBridge.cpp
+++ src/AmsToMqttBridge.cpp
@@ -1,20 +1,6 @@
 /**
- *  Copyright (C) 2022  Gunnar Skjold
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Affero General Public License as published
- *  by the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Affero General Public License for more details.
- *
- *  You should have received a copy of the GNU Affero General Public License
- *  along with this program. If not, see <https://www.gnu.org/licenses/>.
- *
- * @author Gunnar Skjold (@gskjold) gunnar.skjold@gmail.com
+ * @copyright Utilitech AS 2023
+ * License: Fair Source 5
  * 
  * @brief Program for ESP32 and ESP8266 to receive data from AMS electric meters and send to MQTT
  * 

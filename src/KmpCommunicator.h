47a7d4e13b6a37b449725af12ae44b085715c5e8
Gunnar Skjold <gunnar.skjold@gmail.com>
2024-03-10 09:03:06 +0100

    KMP headers

--- src/KmpCommunicator.h
+++ src/KmpCommunicator.h
@@ -24,4 +24,18 @@ public:
 
     HardwareSerial* getHwSerial();
     void rxerr(int err);
+
+private:
+    uint64_t lastUpdate = 0;
+    uint8_t reqNo = 0;
+
+    uint8_t *hanBuffer = NULL;
+    uint16_t hanBufferSize = 0;
+    Stream *hanSerial;
+    #if defined(ESP8266)
+    SoftwareSerial *swSerial = NULL;
+    #endif
+    HardwareSerial *hwSerial = NULL;
+    uint8_t rxBufferErrors = 0;
+
 };

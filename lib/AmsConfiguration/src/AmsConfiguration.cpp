a19901b58f28b5068403c4f21e5e25dfdf149dde
Gunnar Skjold <gunnar.skjold@gmail.com>
2024-04-05 20:16:43 +0200

    Cloud config in config print

--- lib/AmsConfiguration/src/AmsConfiguration.cpp
+++ lib/AmsConfiguration/src/AmsConfiguration.cpp
@@ -6,6 +6,9 @@
 
 #include "AmsConfiguration.h"
 #include "hexutils.h"
+#if defined(ESP32)
+#include "ESPRandom.h"
+#endif
 
 bool AmsConfiguration::getSystemConfig(SystemConfig& config) {
 	EEPROM.begin(EEPROM_SIZE);
@@ -1289,6 +1292,16 @@ void AmsConfiguration::print(Print* debugger)
 	if(getUiConfig(ui)) {
 		debugger->println(F("--UI configuration--"));
 		debugger->printf_P(PSTR("Language:             %s\r\n"), ui.language);
+	}
+
+	CloudConfig cc;
+	if(getCloudConfig(cc)) {
+		String uuid = ESPRandom::uuidToString(cc.clientId);;
+		debugger->println(F("--UI configuration--"));
+		debugger->printf_P(PSTR("Enabled:              %s\r\n"), cc.enabled ? "Yes" : "No");
+		debugger->printf_P(PSTR("Hostname:             %s\r\n"), cc.hostname);
+		debugger->printf_P(PSTR("Client ID:            %s\r\n"), uuid.c_str());
+		debugger->printf_P(PSTR("Interval:             %d\r\n"), cc.interval);
 	}
 
 	debugger->println(F("-----------------------------------------------"));

71be381e1a09ce814edafd89466f43d3a7f52a33
Gunnar Skjold <gunnar.skjold@gmail.com>
2024-04-05 20:54:08 +0200

    Fixed error

--- lib/SvelteUi/src/AmsWebServer.cpp
+++ lib/SvelteUi/src/AmsWebServer.cpp
@@ -2190,7 +2190,7 @@ void AmsWebServer::configFileDownload() {
 	if(includePrice) {
 		PriceServiceConfig price;
 		config->getPriceServiceConfig(price);
-		server.sendContent(buf, snprintf_P(buf, BufferSize, PSTR("priceEnabled %s\n"), price.enabled ? 1 : 0));
+		server.sendContent(buf, snprintf_P(buf, BufferSize, PSTR("priceEnabled %d\n"), price.enabled ? 1 : 0));
 		if(strlen(price.entsoeToken) == 36 && includeSecrets) server.sendContent(buf, snprintf_P(buf, BufferSize, PSTR("priceEntsoeToken %s\n"), price.entsoeToken));
 		server.sendContent(buf, snprintf_P(buf, BufferSize, PSTR("priceArea %s\n"), price.area));
 		server.sendContent(buf, snprintf_P(buf, BufferSize, PSTR("priceCurrency %s\n"), price.currency));

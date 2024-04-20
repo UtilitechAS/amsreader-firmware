43d49f21a3c57ca24255d404883ae970945663c6
Gunnar Skjold <gunnar.skjold@gmail.com>
2024-04-20 10:51:27 +0200

    Changed GPIO dropdowns

--- lib/SvelteUi/app/src/lib/UartSelectOptions.svelte
+++ lib/SvelteUi/app/src/lib/UartSelectOptions.svelte
@@ -12,28 +12,13 @@
     }
 </script>
 
-{#if chip == 'esp32c3'}
-<option value={20}>UART0</option>
-{:else}
-<option value={3}>UART0</option>
-{/if}
 {#if chip == 'esp8266'}
+<option value={3}>UART0</option>
 <option value={113}>UART2</option>
-{/if}
-{#if chip == 'esp32' || chip == 'esp32solo'}
-<option value={9}>UART1</option>
-<option value={16}>UART2</option>
-{/if}
-{#if chip == 'esp32s2' || chip == 'esp32s3' }
-<option value={18}>UART1</option>
 {/if}
 
 {#each {length: gpioMax+1} as _, i}
-    {#if i > 1
-        && !(chip == 'esp32' && (i == 9 || i == 16))
-        && !((chip == 'esp32s2' || chip == 'esp32s3') && i == 18)
-        && !(chip == 'esp8266' && (i == 3 || i == 113))
-    }
+    {#if i > 1 && !(chip == 'esp8266' && (i == 3 || i == 113))}
         <option value={i}>GPIO{i}</option>
     {/if}
 {/each}
